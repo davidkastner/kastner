707f600e6a9f7d7b2df9443d5d6c17140e40bbd2
David Kastner <kastner@mit.edu>
2022-08-22 10:32:59 -0400

    Update spanish translations

--- src/app/core/pipe/localized-date.pipe.ts
+++ src/app/core/pipe/localized-date.pipe.ts
@@ -8,11 +8,11 @@ import { LOCALE_ID, Inject } from "@angular/core";
 })
 export class LocalizedDatePipe implements PipeTransform {
 
-  constructor(@Inject(LOCALE_ID) public locale: string) {}
+  constructor(@Inject(LOCALE_ID) public locale: string) { }
 
   transform(value: any, pattern: string = "mediumDate"): any {
     const datePipe: DatePipe = new DatePipe(this.locale);
     // For now, the translation will be manual, further on, the i18n service will be injected to provide such capability...
-    return datePipe.transform(value, pattern) || (this.locale === "en" ? "Currently" : "Atualmente");
+    return datePipe.transform(value, pattern) || (this.locale === "en" ? "Currently" : "Actualmente");
   }
 }
