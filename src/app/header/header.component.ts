8578755ee0534434d32a413a797bcbf3cc0425b3
David Kastner <kastner@mit.edu>
2022-08-28 17:14:12 -0400

    Update share dialogue

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,11 +9,11 @@ import { NgNavigatorShareService } from "ng-navigator-share";
 })
 
 export class HeaderComponent implements OnInit, AfterViewInit {
-    
+
   private _activeSection: any;
   private _pageXOffset: any;
   private ngNavigatorShareService: NgNavigatorShareService;
-  
+
   hasMenuToggled: boolean;
   faBars: IconDefinition;
   faShareAlt: IconDefinition;
@@ -31,11 +31,11 @@ export class HeaderComponent implements OnInit, AfterViewInit {
   }
 
   // use getter setter to define the properties
-  get activeSection(): any { 
+  get activeSection(): any {
     return this._activeSection;
   }
-  
-  get pageXOffset(): any { 
+
+  get pageXOffset(): any {
     return this._pageXOffset;
   }
 
@@ -51,11 +51,11 @@ export class HeaderComponent implements OnInit, AfterViewInit {
     this.updateNavigation();
   }
 
-  ngAfterViewInit() {    
-      // Share button available only for browsers that do support it.
-      if (this.ngNavigatorShareService.canShare()) {
-        this.shareBtn.nativeElement.style.display = "block";
-      }
+  ngAfterViewInit() {
+    // Share button available only for browsers that do support it.
+    if (this.ngNavigatorShareService.canShare()) {
+      this.shareBtn.nativeElement.style.display = "block";
+    }
   }
 
   ngOnInit(): void {
@@ -66,17 +66,17 @@ export class HeaderComponent implements OnInit, AfterViewInit {
 
   private updateNavigation() {
 
-    if(this._activeSection && this.renderer) {
-      
+    if (this._activeSection && this.renderer) {
+
       // Remove any selected anchor
       const activePreviousElem = this.nav.nativeElement.querySelector("a.active");
-      
-      if(activePreviousElem) {
+
+      if (activePreviousElem) {
         this.renderer.removeClass(activePreviousElem, "active");
       }
 
       const targetElem = this.nav.nativeElement.querySelector(`a[href^="#${this._activeSection}"]`);
-      if(targetElem) {
+      if (targetElem) {
         this.renderer.addClass(targetElem, "active");
       }
     }
@@ -99,14 +99,14 @@ export class HeaderComponent implements OnInit, AfterViewInit {
   }
 
   async share() {
-    try{
+    try {
       await this.ngNavigatorShareService.share({
-        title: "Live Resume - Guilherme Borges Bastos",
-        text: "Hello, I'm a Full-stack Java Web Developer with 10+ years of experience designing web and mobile projects. Find out more in my live-resume!",
+        title: "David W. Kastner",
+        text: "Bioengineer at MIT researching quantum properties of proteins",
         url: "https://kastner.io"
       });
-    } catch(error) {
+    } catch (error) {
       console.log("You app is not shared, reason: ", error);
-    }    
+    }
   }
 }
